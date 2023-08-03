a980a49c6395fbcaaaf4fa6a029a14260b04b739
eugenio-d-tesio <eugeniotesio@gmail.com>
2023-08-03 20:31:04 -0300

    Add empty ProfileDetailsScreen

---
+++ lib/features/pokemon/presentation/screens/pokemon_details_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex/shared/widgets/responsive.dart';
+
+class PokemonDetailsScreen extends StatelessWidget {
+  const PokemonDetailsScreen({
+    required this.id,
+    super.key,
+  });
+
+  final int id;
+
+  @override
+  Widget build(BuildContext context) {
+    return Responsive(
+      desktop: Container(),
+      mobile: Container(),
+    );
+  }
+}
