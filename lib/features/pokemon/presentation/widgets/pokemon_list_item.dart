96b27bd53134a78177341e71a177f234366ce07f
eugenio-d-tesio <eugeniotesio@gmail.com>
2023-08-05 14:43:27 -0300

    Use Text to AppText in PokemonListItemCard

--- lib/features/pokemon/presentation/widgets/pokemon_list_item.dart
+++ lib/features/pokemon/presentation/widgets/pokemon_list_item.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:pokedex/features/pokemon/domain/entities/pokemon_list.dart';
+import 'package:pokedex/shared/widgets/app_text.dart';
 import 'package:shimmer/shimmer.dart';
 
 class PokemonListItemCard extends StatelessWidget {
@@ -34,7 +35,7 @@ class PokemonListItemCard extends StatelessWidget {
           errorWidget: (context, url, error) => const Icon(Icons.error),
           placeholder: (context, url) => const Icon(Icons.image),
         ),
-        title: Text(pokemonListItem.name),
+        title: AppText(pokemonListItem.name),
       ),
     );
   }
