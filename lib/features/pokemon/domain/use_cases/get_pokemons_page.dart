a7e0e21a9cc6f4a4b47472b066b9c52f0ffd6bb5
eugenio-d-tesio <eugeniotesio@gmail.com>
2023-08-05 13:08:15 -0300

    Add GetPockemonsPage use case

---
+++ lib/features/pokemon/domain/use_cases/get_pokemons_page.dart
@@ -0,0 +1,23 @@
+import 'package:pokedex/core/http_client/domain/http_client_exception.dart';
+import 'package:pokedex/features/pokemon/domain/entities/pokemon_list.dart';
+import 'package:pokedex/features/pokemon/domain/repositories/pokemon_repository.dart';
+
+class GetPockemonsPage {
+  GetPockemonsPage({required this.pokemonRepository});
+
+  final PokemonRepository pokemonRepository;
+
+  /// Get pokemons page from the repository.
+  /// The parameter [page] starts at 1.
+  Future<(PokemonList?, HttpClientException?)> call(int page) async {
+    if (page < 1) {
+      return (null, null);
+    }
+    const limit = 50;
+    final offset = limit * (page - 1);
+    return pokemonRepository.fetchPokemons(
+      limit: limit,
+      offset: offset,
+    );
+  }
+}
