7143ea24a2e8a81684bc7bec412ab265262df47a
eugenio-d-tesio <eugeniotesio@gmail.com>
2023-08-05 14:42:03 -0300

    Add PokedexAppBar

---
+++ lib/shared/widgets/app_bar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pokedex/core/constants/sizes.dart';
+import 'package:pokedex/features/dark_mode/presentation/widgets/dark_mode_switch.dart';
+
+class PokedexAppBar extends ConsumerWidget {
+  const PokedexAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return AppBar(
+      scrolledUnderElevation: 0,
+      backgroundColor: Theme.of(context).colorScheme.tertiary.withAlpha(40),
+      centerTitle: false,
+      titleTextStyle: Theme.of(context).textTheme.titleLarge?.copyWith(
+            fontWeight: FontWeight.bold,
+          ),
+      title: const Row(
+        children: [
+          Text(
+            'Pokedex',
+            style: TextStyle(fontFamily: 'Pokemon Solid'),
+          ),
+        ],
+      ),
+      actions: const [
+        Row(
+          children: [
+            DarkModeSwitch(),
+            AppGaps.gapW8,
+          ],
+        ),
+      ],
+    );
+  }
+}
