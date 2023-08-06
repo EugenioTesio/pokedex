d713049465dd50ec5488649232e70770e338cca3
eugenio-d-tesio <eugeniotesio@gmail.com>
2023-08-06 11:53:03 -0300

    Images from pkparaiso

--- lib/features/pokemon/presentation/widgets/pokemon_list_item.dart
+++ lib/features/pokemon/presentation/widgets/pokemon_list_item.dart
@@ -1,5 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:image_network/image_network.dart';
 import 'package:pokedex/features/pokemon/domain/entities/pokemon_list.dart';
 import 'package:pokedex/shared/widgets/app_text.dart';
 import 'package:shimmer/shimmer.dart';
@@ -17,26 +19,66 @@ class PokemonListItemCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      borderOnForeground: false,
       elevation: 10,
+      shadowColor: Theme.of(context).colorScheme.tertiary,
       child: ListTile(
         onTap: () => onTap?.call(pokemonListItem),
-        leading: CachedNetworkImage(
-          imageUrl: pokemonListItem.url,
+        leading: Container(
+          padding: EdgeInsets.all(8),
           width: 50,
           height: 50,
-          progressIndicatorBuilder: (context, url, progress) =>
-              Shimmer.fromColors(
-            baseColor: Colors.grey,
-            highlightColor: Colors.white,
-            child: const SizedBox.expand(
-              child: Icon(Icons.image),
-            ),
-          ),
-          errorWidget: (context, url, error) => const Icon(Icons.error),
+          child: kIsWeb
+              ? ImageNetwork(
+                  image: parseUrlToPkparaisoImage(pokemonListItem.url),
+                  width: 50,
+                  height: 50,
+                  onLoading: Shimmer.fromColors(
+                    baseColor: Colors.grey,
+                    highlightColor: Colors.white,
+                    child: const SizedBox.expand(
+                      child: Icon(Icons.image),
+                    ),
+                  ),
+                  fitWeb: BoxFitWeb.scaleDown,
+                  borderRadius: BorderRadius.circular(10),
+                )
+              : CachedNetworkImage(
+                  imageUrl: pokemonListItem.url,
+                  width: 50,
+                  height: 50,
+                  progressIndicatorBuilder: (context, url, progress) =>
+                      Shimmer.fromColors(
+                    baseColor: Colors.grey,
+                    highlightColor: Colors.white,
+                    child: const SizedBox.expand(
+                      child: Icon(Icons.image),
+                    ),
+                  ),
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
+                  imageBuilder: (context, imageProvider) {
+                    return DecoratedBox(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Image(
+                        image: imageProvider,
+                        fit: BoxFit.scaleDown,
+                      ),
+                    );
+                  },
+                ),
         ),
         title: AppText(pokemonListItem.name),
       ),
     );
+  }
+
+  /// Parse the url to get the image from pkparaiso
+  String parseUrlToPkparaisoImage(String url) {
+    final regex = RegExp(r'\/(\d+)\/$');
+    final match = regex.firstMatch(url);
+    final number = int.parse(match?.group(1) ?? '');
+    final numberString = number.toString().padLeft(3, '0');
+    return 'https://www.pkparaiso.com/imagenes/pokedex/pokemon/$numberString.png';
   }
 }
