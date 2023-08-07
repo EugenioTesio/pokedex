5bc24951d912d49dacabf7bdadd53acc7af88f9b
eugenio-d-tesio <eugeniotesio@gmail.com>
2023-08-07 16:35:10 -0300

    Add Fetch Pokemon use case

---
+++ lib/features/pokemon/domain/use_cases/fetch_pokemon_details.dart
@@ -0,0 +1,15 @@
+import 'package:pokedex/core/http_client/domain/http_client_exception.dart';
+import 'package:pokedex/features/pokemon/domain/entities/pokemon_details.dart';
+import 'package:pokedex/features/pokemon/domain/repositories/pokemon_repository.dart';
+
+class FetchPokemonDetails {
+  FetchPokemonDetails({
+    required this.pokemonRepository,
+  });
+
+  final PokemonRepository pokemonRepository;
+
+  Future<(PokemonDetails?, AppException?)> call(String name) async {
+    return pokemonRepository.fetchPokemonDetails(name);
+  }
+}
