6f2de845f3c97944928b61c8d151ac254bb01cea
eugenio-d-tesio <eugeniotesio@gmail.com>
2023-08-11 11:55:37 -0300

    Fix app_test

--- test/app_test.dart
+++ test/app_test.dart
@@ -4,7 +4,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:pokedex/app.dart';
 import 'package:pokedex/features/pokemon/data/repositories/fake_pokemon_repository.dart';
 import 'package:pokedex/features/pokemon/domain/providers/pokemon_providers.dart';
-import 'package:pokedex/features/pokemon/presentation/screens/pokemon_list_desktop.dart';
+import 'package:pokedex/features/pokemon/presentation/screens/pokemon_list_screen.dart';
 import 'package:pokedex/features/splash_screen/presentation/splash_screen.dart';
 import 'package:pokedex/shared/image_cacher/data/repositories/fake_image_cacher.dart';
 import 'package:pokedex/shared/image_cacher/domain/providers/image_cacher_provider.dart';
@@ -26,9 +26,10 @@ void main() {
     );
 
     expect(find.byType(SplashScreen), findsOneWidget);
+    expect(find.byType(PokemonListScreen), findsNothing);
     await tester.pump(const Duration(seconds: 2));
     await tester.pump(const Duration(seconds: 2));
     expect(find.byType(SplashScreen), findsNothing);
-    expect(find.byType(PokemonListDesktop), findsOneWidget);
+    expect(find.byType(PokemonListScreen), findsOneWidget);
   });
 }
